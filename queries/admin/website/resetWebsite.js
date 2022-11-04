157ceed0ac7c67b68eaf7940cd0773deb0da49c8
Francis Cao <franciscao@gmail.com>
2022-11-04 16:22:06 -0700

    update schema files and reset website

--- queries/admin/website/resetWebsite.js
+++ queries/admin/website/resetWebsite.js
@@ -3,6 +3,10 @@ import prisma from 'lib/prisma';
 export async function resetWebsite(id) {
   const { client, transaction } = prisma;
 
+  const website = await prisma.client.website.findUnique({
+    id,
+  });
+
   return transaction([
     client.pageview.deleteMany({
       where: { session: { website: { id } } },
@@ -15,6 +19,12 @@ export async function resetWebsite(id) {
     }),
     client.session.deleteMany({
       where: { website: { id } },
+    }),
+    client.website.update({
+      where: {
+        id,
+      },
+      revId: website.revId + 1,
     }),
   ]);
 }
